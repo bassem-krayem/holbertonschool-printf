c15d5051e138d5ac0793f88eb784dce0b157c02c
bassem <104699079+bassem-krayem@users.noreply.github.com>
2024-03-30 16:55:57 +0100

    Update 0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -1,59 +1,50 @@
-	#include "main.h"
-	
-	/**
-	 * _printf - cloning the standard printf function
-	 * @format: pointer to string
-	 * @...: list of arguments passed to the function
-	 *
-	 * Return: length of the string
-	 */
-	int _printf(const char *format, ...)
+#include "main.h"
+
+/**
+ * _printf - cloning the standard printf function
+ * @format: pointer to string
+ * @...: list of arguments passed to the function
+ *
+ * Return: length of the string
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0, i = 0, j = 0;
+	spec_t spec_list[] = {
+		{'c', print_char}, {'s', print_string},
+		{'%', print_percent}, {'d', print_decimal},
+		{'i', print_decimal}, {'u', print_unsigned},
+		{'o', print_octal}, {'X', print_HEX},
+		{'x', print_hex}, {'p', print_address},
+		{'\0', NULL}
+	};
+
+	va_start(args, format);
+	if (!format)
+		return (-1);
+	while (format && format[i])
 	{
-		va_list args;
-		int count = 0, i = 0, j = 0;
-	
-		spec_t spec_list[] = {
-			{'c', print_char}, {'s', print_string},
-			{'%', print_percent}, {'d', print_decimal},
-			{'i', print_decimal}, {'u', print_unsigned},
-			{'o', print_octal}, {'X', print_HEX},
-			{'x', print_hex}, {'p', print_address},
-			{'\0', NULL}
-		};
-	
-		va_start(args, format);
-		if (format == NULL)
-			return (-1);
-		while (format != NULL && format[i] != '\0')
+		if (format[i] == '%' && format[i + 1])
 		{
-			if (format[i] == '%')
+			while (spec_list[j].c)
 			{
-				if (format[i + 1] == '\0')
-					return (-1);
-				while (spec_list[j].c != '\0')
+				if (format[i + 1] == spec_list[j].c)
 				{
-					if (format[i + 1] == spec_list[j].c)
-					{
-						count += spec_list[j].spec(args);
-						break;
-					}
-					j++;
+					count += spec_list[j].spec(args);
+					break;
 				}
-				if (spec_list[j].c == '\0')
-				{
-					count += _putchar(format[i]);
-					count += _putchar(format[i + 1]);
-				}
-				j = 0;
-				i++;
+				j++;
 			}
-			else
-			{
-				_putchar(format[i]);
-				count++;
-			}
+			if (!spec_list[j].c)
+				count += _putchar(format[i]) + _putchar(format[i + 1]);
+			j = 0;
 			i++;
 		}
-		va_end(args);
-		return (count);
+		else
+			count += _putchar(format[i]);
+		i++;
 	}
+	va_end(args);
+	return (count);
+}
