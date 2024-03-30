19782d01f3798537eac5a4fce665d2e9fd695e84
bassem-krayem <bassem.krayem09@gmail.com>
2024-03-30 18:06:14 +0100

    task0 and 1

--- 0-printf.c
+++ 0-printf.c
@@ -11,45 +11,45 @@
 int _printf(const char * const format, ...)
 {
 	va_list args;
-	int count = 0, i = 0, j = 0;
+	int count = 0, j = 0;
 	spec_t spec_list[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'d', print_decimal}, {'i', print_decimal}, {'u', print_unsigned},
 		{'o', print_octal}, {'X', print_HEX}, {'x', print_hex},
-		{'p', print_address}, {'\0', NULL}};
+		{'p', print_address}, {'\0', NULL}
+	};
 
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	while (format != NULL && format[i] != '\0')
-{
+	
+	for (int i = 0; format[i] != '\0'; i++)
+	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
 				return (-1);
-			while (spec_list[j].c != '\0')
+			for (j = 0; spec_list[j].c != '\0'; j++)
 			{
 				if (format[i + 1] == spec_list[j].c)
 				{
 					count += spec_list[j].spec(args);
 					break;
 				}
-				j++;
 			}
 			if (spec_list[j].c == '\0')
 			{
 				count += _putchar(format[i]);
 				count += _putchar(format[i + 1]);
 			}
-			j = 0;
 			i++;
-		} else
+		}
+		else
 		{
 			_putchar(format[i]);
 			count++;
 		}
-		i++;
-		}
+	}
 	va_end(args);
 	return (count);
 }
